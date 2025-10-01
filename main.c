63bda70e01d6a0f7f8e7d21cdcf07d5c3ceb8718
Draghiciu Alexandru <adraghiciu29@gmail.com>
2025-10-01 15:19:35 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void schimb(int *a, int *b)
+{
+    int aux = *a;
+    *a = *b;
+    *b = aux;
+}
+
+void Selection_Sort(int v[], int n)
+{
+    for (int poz = 0; poz < n; poz++)
+    {
+        for (int i = 0; i < 5; i++)
+            printf("%d ", v[i]);
+        printf("\n");
+
+        int minim = v[poz], poz_min = poz;
+        for (int i = poz + 1; i < n; i++)
+            if (v[i] < minim)
+            {
+                minim = v[i];
+                poz_min = i;
+            }
+
+        schimb(&v[poz], &v[poz_min]);
+    }
+}
+
+int main()
+{
+    int v[] = {47, 23, 12, 17, 30};
+    Selection_Sort(v, 5);
+
+    return 0;
+}
